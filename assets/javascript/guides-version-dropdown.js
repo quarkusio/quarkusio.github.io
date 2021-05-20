4f7945b3d46e4341adcadb72b28a2c61500223e7
Guillaume Smet <guillaume.smet@gmail.com>
2021-05-20 13:17:45 +0200

    James renamed the id of the selector so we need to take it into account

--- assets/javascript/guides-version-dropdown.js
+++ assets/javascript/guides-version-dropdown.js
@@ -8,4 +8,14 @@ $(document).ready(function() {
       window.location.href = '/version/' + this.value + guideUrl;
     }
   });
+
+  $('#guide-version-dropdown').on('change', function() {
+    var guideUrl = window.location.href.substring(window.location.href.indexOf("/guides/"));
+
+    if (this.value == 'latest') {
+      window.location.href = guideUrl;
+    } else {
+      window.location.href = '/version/' + this.value + guideUrl;
+    }
+  });
 });
