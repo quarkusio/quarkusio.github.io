966f67c7b578cdecf7cd3ae1914c4883770dc1ea
Holly Cummins <hcummins@redhat.com>
2024-09-03 16:43:29 +0100

    Mark the highest version as 'main'

--- _plugins/version_permalink.rb
+++ _plugins/version_permalink.rb
@@ -19,20 +19,28 @@ module VersionPermalink
 
       number_pattern = /^[0-9.]+$/
 
+      j = 0
+      # Add a marker for things that don't have a version, because liquid filters are not as flexible as we'd like
+      # This has to happen before we do any conversion of 'main'
+      collection.docs.filter { |doc| ! doc.data["version"].match?(number_pattern) }.each { |doc|
+        data = doc.data
+        j +=1
+        data.merge!('unversion_counter' => j)
+      }
+
       # Also add a version scalar that we can use for sorting
       i = 0
       collection.docs.filter { |doc| doc.data["version"].match?(number_pattern) }.sort_by { |doc| Gem::Version.new(doc.data["version"]) }.reverse.each { |doc|
         data = doc.data
         i +=1
         data.merge!('version_counter' => i)
-      }
 
-      j = 0
-      # Add a marker for things that don't have a version, because liquid filters are not as flexible as we'd like
-      collection.docs.filter { |doc| ! doc.data["version"].match?(number_pattern) }.each { |doc|
-        data = doc.data
-        j +=1
-        data.merge!('unversion_counter' => j)
+
+        # Mark the latest release as main
+        if i == 1
+          data.merge!('proposed_version' => data["version"])
+          data.merge!('version' => "main")
+        end
       }
 
     end
