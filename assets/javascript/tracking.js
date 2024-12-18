e03caf4cf313a127ca87415cde5313136709c8f8
James Cobb <jcobb@redhat.com>
2024-12-18 11:04:44 -0600

    refine url and change url in .js file

--- assets/javascript/tracking.js
+++ assets/javascript/tracking.js
@@ -3,7 +3,7 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="https://route-default-test-mscherer-matamo.apps.ospo-osci.z3b1.p1.openshiftapps.com/";
+  var u="https://analytics.ossupstream.org/";
   _paq.push(['setTrackerUrl', u+'matomo.php']);
   _paq.push(['setSiteId', '2']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
