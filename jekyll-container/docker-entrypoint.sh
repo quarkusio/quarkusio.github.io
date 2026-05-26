dd1fae4927253dbb848524a2f603b5654c0abc6f
GitHub Action <action@github.com>
2026-05-26 08:48:01 +0000

    deploy: d2f5668c379c46a9ffb4675f6f900e1680398799

---
+++ jekyll-container/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [ ! -f Gemfile ]; then
+  echo "NOTE: hmm, I don't see a Gemfile so I don't think there's a jekyll site here"
+  echo "Either you didn't mount a volume, or you mounted it incorrectly."
+  echo "Be sure you're in your jekyll site root and use something like this to launch"
+  echo ""
+  echo "docker compose up"
+  echo ""
+  exit 1
+fi
+
+bundle install --retry 5 --jobs 20
+
+exec "$@"
